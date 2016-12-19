f9eb6e1c95ec82beb33fc9ffb17dc2827b3bd9f3
Thomas McNamara <G00314495@gmit.ie>
2016-12-19 21:06:04 +0000

    'Higher complexity' math for HoL Game

--- 4thYearAppliedProject/HigherOrLower.xaml.cs
+++ 4thYearAppliedProject/HigherOrLower.xaml.cs
@@ -32,13 +32,18 @@ namespace AppliedProject4thYear
 
 
         int HoLScore = 0;
-        Random random1 = new Random();
-        Random random2 = new Random();
+        Random random1 = new Random(200);
+        Random random2 = new Random(205);
+        Random random3 = new Random(210);
+        Random random4 = new Random(220);
+
         int Lives = 5;
-        int randomNumber1; // always hit the top one first
+        int randomNumber1;
         int randomNumber2;
-
-
+        int randomNumber3;
+        int randomNumber4;
+        private int randomNumber1And2;
+        private int randomNumber3And4;
 
         private void PSGBackBtn_Click(object sender, RoutedEventArgs e)
         {
@@ -65,14 +70,27 @@ namespace AppliedProject4thYear
                 }
             }
 
+            Random random1 = new Random(randomNumber1 * randomNumber2);
+            Random random2 = new Random(randomNumber2 * randomNumber3);
+            Random random3 = new Random(randomNumber3 * randomNumber4);
+            Random random4 = new Random(randomNumber1 * randomNumber4);
 
             randomNumber1 = random1.Next(1, 101);
 
-            Random random2 = new Random();
+            //Random random2 = new Random();
             randomNumber2 = random2.Next(1, 101);
 
             btnFirstChoice.Content = randomNumber1;
             btnSecondChoice.Content = randomNumber2;
+
+            // Bottom Question
+            randomNumber4 = random4.Next(1, 101);
+
+            //Random random3 = new Random();
+            randomNumber3 = random3.Next(1, 101);
+
+            btnChoice3.Content = randomNumber3;
+            btnChoice4.Content = randomNumber4;
 
         }
 
@@ -96,14 +114,29 @@ namespace AppliedProject4thYear
                 }
             }
 
-
+            Random random1 = new Random(randomNumber1 * randomNumber2);
+            Random random2 = new Random(randomNumber2 * randomNumber3);
+            Random random3 = new Random(randomNumber3 * randomNumber4);
+            Random random4 = new Random(randomNumber1 * randomNumber4);
+            // Top Question
             randomNumber2 = random2.Next(1, 101);
 
-            Random random1 = new Random();
+           // Random random1 = new Random();
             randomNumber1 = random1.Next(1, 101);
 
             btnFirstChoice.Content = randomNumber1;
             btnSecondChoice.Content = randomNumber2;
+
+
+            // Bottom Question
+            randomNumber4 = random4.Next(1, 101);
+
+            //Random random3 = new Random();
+            randomNumber3 = random3.Next(1, 101);
+
+            btnChoice3.Content = randomNumber3;
+            btnChoice4.Content = randomNumber4;
+
 
         }
 
@@ -111,17 +144,121 @@ namespace AppliedProject4thYear
         {
             btnStart.Visibility = Visibility.Collapsed;
             txtLives.Visibility = Visibility.Visible;
+            btnChoiceOne.Visibility = Visibility.Visible;
+            btnChoiceTwo.Visibility = Visibility.Visible;
+            btnChoice3.Visibility = Visibility.Visible;
+            btnChoice4.Visibility = Visibility.Visible;
+            btnFirstChoice.Visibility = Visibility.Visible;
+            btnSecondChoice.Visibility = Visibility.Visible;
+            btnFirstChoice_Copy.Visibility = Visibility.Visible;
+            btnFirstChoice_Copy1.Visibility = Visibility.Visible;
+
+
+
             txtLives.Text = Lives.ToString();
 
             randomNumber2 = random2.Next(1, 101);
-            Random random1 = new Random();
+            //Random random1 = new Random();
             randomNumber1 = random1.Next(1, 101);
+
+            btnFirstChoice.Content = (randomNumber1);
+            btnSecondChoice.Content = randomNumber2;
+
+            // Bottom Question
+            randomNumber4 = random4.Next(1, 101);
+
+            //Random random3 = new Random();
+            randomNumber3 = random3.Next(1, 101);
+
+            btnChoice3.Content = randomNumber3;
+            btnChoice4.Content = randomNumber4;
+
+        }
+
+        private async void btnChoiceOne_Click(object sender, RoutedEventArgs e)
+        {
+            randomNumber1And2 = randomNumber1 + randomNumber2;
+            randomNumber3And4 = randomNumber3 + randomNumber4;
+
+            if (randomNumber1And2 > randomNumber3And4)
+            {
+                HoLScore++;
+                displayScore.Text = HoLScore.ToString();
+            }
+            else if (randomNumber1And2 < randomNumber3And4)
+            {
+                Lives--;
+                txtLives.Text = Lives.ToString();
+                if (Lives == 0)
+                {
+                    var dialog = new Windows.UI.Popups.MessageDialog(
+                    "You have lost! You scored a total of: " + HoLScore);
+                    var result = await dialog.ShowAsync();
+                    this.Frame.Navigate(typeof(MainPage), null);
+                }
+            }
+
+
+            randomNumber1 = random1.Next(1, 101);
+
+            //Random random2 = new Random();
+            randomNumber2 = random2.Next(1, 101);
 
             btnFirstChoice.Content = randomNumber1;
             btnSecondChoice.Content = randomNumber2;
-            
+
+            // Bottom Question
+            randomNumber4 = random4.Next(1, 101);
+
+            //Random random3 = new Random();
+            randomNumber3 = random3.Next(1, 101);
+
+            btnChoice3.Content = randomNumber3;
+            btnChoice4.Content = randomNumber4;
+
         }
 
-        
+        private async void btnChoiceTwo_Click(object sender, RoutedEventArgs e)
+        {
+            randomNumber1And2 = randomNumber1 + randomNumber2;
+            randomNumber3And4 = randomNumber3 + randomNumber4;
+
+            if (randomNumber3And4 > randomNumber1And2)
+            {
+                HoLScore++;
+                displayScore.Text = HoLScore.ToString();
+            }
+            else if (randomNumber3And4 < randomNumber1And2)
+            {
+                Lives--;
+                txtLives.Text = Lives.ToString();
+                if (Lives == 0)
+                {
+                    var dialog = new Windows.UI.Popups.MessageDialog(
+                    "You have lost! You scored a total of: " + HoLScore);
+                    var result = await dialog.ShowAsync();
+                    this.Frame.Navigate(typeof(MainPage), null);
+                }
+            }
+
+
+            randomNumber1 = random1.Next(1, 101);
+
+            //Random random2 = new Random();
+            randomNumber2 = random2.Next(1, 101);
+
+            btnFirstChoice.Content = randomNumber1;
+            btnSecondChoice.Content = randomNumber2;
+
+            // Bottom Question
+            randomNumber4 = random4.Next(1, 101);
+
+            //Random random3 = new Random();
+            randomNumber3 = random3.Next(1, 101);
+
+            btnChoice3.Content = randomNumber3;
+            btnChoice4.Content = randomNumber4;
+
+        }
     }
 }
